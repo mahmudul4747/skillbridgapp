4ee862384c35753ce51b61785ae4a244e8378ddc
mahmudul4747 <mahmudulimran396@gmail.com>
2026-07-27 23:56:54 +0600

    main file update

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,14 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     const ProviderScope(
@@ -21,7 +24,10 @@ class SkillBridgeApp extends ConsumerWidget {
   const SkillBridgeApp({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(
+    BuildContext context,
+    WidgetRef ref,
+  ) {
     final router = ref.watch(appRouterProvider);
 
     return MaterialApp.router(
